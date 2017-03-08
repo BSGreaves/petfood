e6db868564fd977882049265feeaf4e8c9a1490d
Ben Greaves <bsgreaves@gmail.com>
2017-03-07 21:58:02 -0600

    Done

---
+++ main.js
@@ -0,0 +1,57 @@
+const outputDiv = document.getElementById("outputDiv");
+
+function printToDOM (){
+	let data = JSON.parse(this.responseText);
+	console.log(data);
+	let ops = ""; //outputstring
+	let dogBrands = data.dog_brands;
+	let catBrands = data.cat_brands;
+	for (let i = 0; i < dogBrands.length; i++) {
+		ops += `<div class="col-sm-6 col-md-4 food"><div class="caption">`
+		let currBrand = dogBrands[i];
+		ops += `<h2>${currBrand.name}</h2>`
+		for (let j = 0; j < currBrand.types.length; j++) {
+			let currType = currBrand.types[j];
+			ops += `<h4>${currType.type}</h4>`;
+			for (let k = 0; k < currType.volumes.length; k++) {
+				let currVolume = currType.volumes[k];
+				ops += `<p>${currVolume.name}: $${currVolume.price}</p>`;
+			}
+
+		}
+		ops += `</div></div>`
+	}
+	for (let i = 0; i < catBrands.length; i++) {
+		ops += `<div class="col-sm-6 col-md-4 food"><div class="caption">`
+		let currBrand = catBrands[i];
+		ops += `<h2>${currBrand.name}</h2>`
+		ops += `<p>For breeds such as: ${currBrand.breeds}</p>`
+		for (let j = 0; j < currBrand.types.length; j++) {
+			let currType = currBrand.types[j];
+			ops += `<h4>${currType.type}</h4>`;
+			for (let k = 0; k < currType.volumes.length; k++) {
+				let currVolume = currType.volumes[k];
+				ops += `<p>${currVolume.name}: $${currVolume.price}</p>`;
+			}
+
+		}
+		ops += `</div></div>`
+	}
+
+	// ops += `
+	// 	<div class="col-sm-6 col-md-4">
+	// 		<div class="caption">
+	// 			<h2>${dogs}</h2>
+	// 			<h3>Thumbnail label</h3>
+
+	// 			<p>Test</p>
+	// 		</div>
+	// 	</div>`
+	outputDiv.innerHTML = ops;
+}
+
+var dataReq = new XMLHttpRequest();
+dataReq.addEventListener("load", printToDOM);
+dataReq.addEventListener("error", function(){console.log("JSON Error");});
+dataReq.open("GET", "petfood.json");
+dataReq.send();
